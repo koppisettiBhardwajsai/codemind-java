fdaec55f791da03ae8aef65b4e7b1a0f6bfa3d2c
koppisettiBhardwajsai <120609887+koppisettiBhardwajsai@users.noreply.github.com>
2024-01-21 10:09:06 +0530

    import java.util.*; public class ReachHome {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         for(int i=x;i<=y;i++)         {             System.out.printf("%d ",i);         }     } }

---
+++ A_to_B.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ReachHome
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        for(int i=x;i<=y;i++)
+        {
+            System.out.printf("%d ",i);
+        }
+    }
+}
